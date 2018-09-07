1d0bcb970ecb4b04e7085042275b57920e527abd
Georgia Tardy <georgiatardyart@gmail.com>
2018-09-07 12:25:31 -0400

    started building search bar component, search bar rendered in browser

--- src/index.js
+++ src/index.js
@@ -1,13 +1,20 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import SearchBar from './components/search_bar';
 
 const API_KEY = 'AIzaSyCwhXiJEZQtSJRZc1tFjv70ZWd3ETM2CI4';
 
 
 //Create a new component. This component should produce some HTML//
 const App = () => {
-  return <div>Hi!</div>;
+  return (
+  <div>
+    <SearchBar />
+  </div>
+);
 }
+
+
 
 //Take this component's generated HTML and put on the page (in the DOM)//
 ReactDOM.render(<App />, document.querySelector('.container'));
