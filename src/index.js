f6bee1408bd56c3844d227955d039604aac9a03a
Georgia Tardy <georgiatardyart@gmail.com>
2018-09-25 10:43:23 -0400

    called api to searchbar component to enable youtube searches

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 
 const API_KEY = 'AIzaSyCwhXiJEZQtSJRZc1tFjv70ZWd3ETM2CI4';
+
+YTSearch({key:API_KEY, term:'surfboards'}, function(data) {
+console.log(data);
+})
 
 
 //Create a new component. This component should produce some HTML//
